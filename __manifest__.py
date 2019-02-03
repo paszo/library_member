0ac555e900f4ebc2ae8d2884c07bf7996880ff14
Paszo <paszo2008@gmail.com>
2019-02-03 13:20:39 +0100

    [ADD] Added library_member menu

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
     'views/book_view.xml',
     'security/library_security.xml',
     'security/ir.model.access.csv',
+    'views/library_menu.xml',
 ],
 'depends': ['library_app'],
 'application': False,
