2ac8679ce362b96b8d93a2c73d5dc5089494b7d6
Paszo <paszo2008@gmail.com>
2019-02-03 09:19:27 +0100

    [ADD] Added is available field to the form view

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,9 @@
 'name': 'Library Members and Borrowing',
 'description': 'User library cards for people to borrow books',
 'author': 'Daniel Reis',
+'data': [
+    'views/book_view.xml',
+],
 'depends': ['library_app'],
 'application': False,
 }
