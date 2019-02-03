0121014d396a3b8e5035c03820fd3ac5d1782c50
Paszo <paszo2008@gmail.com>
2019-02-03 08:27:26 +0100

    [ADD] Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+'name': 'Library Members and Borrowing',
+'description': 'User library cards for people to borrow books',
+'author': 'Daniel Reis',
+'depends': ['library_app'],
+'application': False,
+}
